c511ba01bc5b487f3acb933d0454d7bb0c51014b
Yashika0902 <sumati0902@gmail.com>
2025-08-27 05:04:39 +0530

    Initial Commit

---
+++ Sample_three/Sample_threeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_threeApp.swift
+//  Sample_three
+//
+//  Created by Yashika Kushwaha on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Sample_threeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sample_threeTests/Sample_threeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_threeTests.swift
+//  Sample_threeTests
+//
+//  Created by Yashika Kushwaha on 27/08/25.
+//
+
+import Testing
+@testable import Sample_three
+
+struct Sample_threeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
